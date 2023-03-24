551f03f545a7555bef1bf71bee8128ed79301921
watahiki606 <v10acdict@gmail.com>
2023-03-24 22:22:32 +0000

    init

---
+++ postCreateCommand.sh
@@ -0,0 +1,9 @@
+npm install
+npm install -g @aws-amplify/cli@10.0.0
+pip3 install --upgrade pip
+pip3 install awscli --upgrade --user
+pip3 install boto3
+pip3 install autopep8
+pip3 install pytest
+pip3 install pytest-mock
+pip3 install pytest-cov
