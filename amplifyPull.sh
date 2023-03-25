06626a495810c3b36f947a03de51fb2fb090aaa9
watahiki606 <v10acdict@gmail.com>
2023-03-25 05:57:55 +0000

    amplifyPull

---
+++ amplifyPull.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+IFS='|'
+REACTCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"build\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"accessKeyId\":\"$1\",\
+\"secretAccessKey\":\"$2\",\
+\"region\":\"$3\"\
+}"
+
+        
+AMPLIFY="{\
+\"projectName\":\"amplifytest\",\
+\"appId\":\"d3h268v90nd38j\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"code\"\
+}"
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"react\",\
+\"config\":$REACTCONFIG\
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+amplify pull \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
