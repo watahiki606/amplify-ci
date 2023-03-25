57a087f8f8b2e2b246aefb64e1455894534c82a9
watahiki606 <v10acdict@gmail.com>
2023-03-25 00:20:58 +0000

    test

--- amplify/backend/awscloudformation/override.ts
+++ amplify/backend/awscloudformation/override.ts
@@ -1,33 +1,39 @@
 import { AmplifyRootStackTemplate } from "@aws-amplify/cli-extensibility-helper";
 
 export function override(resources: AmplifyRootStackTemplate) {
-  const loggerPolicy = {
-    policyDocument: {
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Effect: "Allow",
-          Action: "logs:PutLogEvents",
-          Resource: `arn:aws:logs:*:*:log-group:*`,
-        },
-        {
-          Effect: "Allow",
-          Action: [
-            "logs:CreateLogStream",
-            "logs:CreateLogGroup",
-            "logs:DescribeLogStreams",
-          ],
-          Resource: `arn:aws:logs:*:*:log-group:*`,
-        },
-        {
-          Effect: "Allow",
-          Action: ["logs:DescribeLogGroups"],
-          Resource: `arn:aws:logs:*:*:log-group:*`,
-        },
-      ],
-    },
-    policyName: "amplifyLoggerCWLogsPolicy",
-  };
+  const unauthRole = resources.unauthRole;
+  const basePolicies = Array.isArray(unauthRole.policies)
+    ? unauthRole.policies
+    : [unauthRole.policies];
+  unauthRole.policies = [
+    ...basePolicies,
 
-  resources.unauthRole.policies = [loggerPolicy];
+    {
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Action: "logs:PutLogEvents",
+            Resource: `arn:aws:logs:*:*:log-group:*`,
+          },
+          {
+            Effect: "Allow",
+            Action: [
+              "logs:CreateLogStream",
+              "logs:CreateLogGroup",
+              "logs:DescribeLogStreams",
+            ],
+            Resource: `arn:aws:logs:*:*:log-group:*`,
+          },
+          {
+            Effect: "Allow",
+            Action: ["logs:DescribeLogGroups"],
+            Resource: `arn:aws:logs:*:*:log-group:*`,
+          },
+        ],
+      },
+      policyName: "amplifyLoggerCWLogsPolicy",
+    },
+  ];
 }
